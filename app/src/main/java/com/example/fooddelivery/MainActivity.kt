60759e98aa0444a72ee61f9943a3c0798914beb8
vikas <vikaspant990@gmail.com>
2023-04-10 13:19:14 +0530

    Final Commit

---
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fooddelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+public class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
